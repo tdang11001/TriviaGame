b24fa386da109120121017fbed202836aaffe1d4
Tony Dang <tdang11001@gmail.com>
2018-12-18 20:20:27 -0500

    v4

--- assets/js/game.js
+++ assets/js/game.js
@@ -1,19 +1,6 @@
 
 $(document).ready(function() {
 
-    // var question1 = ["Bulbasaur", "Ivysaur", "Venusaur", "Charmander"];
-    // var question2 = ["Pichu", "Jolteon", "Raichu", "Electabuzz"];
-    // var question3 = ["Fire", "Water", "Grass", "Electric"];
-    // var question4 = ["Water", "Fire", "Electric", "Grass"];
-    // var question5 = ["Fire", "Flying", "Water", "Rock"];
-    // var question6 = ["Water", "Fire", "Grass", "Electric"];
-
-    // var q1Answer = "Bulbasaur";
-    // var q2Answer = "Raichu";
-    // var q3Answer = "Electric";
-    // var q4Answer = "Water";
-    // var q5Answer = "Rock";
-    // var q6Answer = "Water";
 
     var answers = ["Bulbasaur", "Raichu", "Electric", "Water", "Rock", "Water"];
 
@@ -31,14 +18,7 @@ $(document).ready(function() {
 
     function checkAnswers() {
         event.preventDefault();
-        // var q1 = document.getElementById("q1");
-        // var q1 = $("input[name='q1']:checked").val();
-        // var q2 = document.getElementById("q2").value;
-        // var q3 = document.getElementById("q3").value;
-        // var q4 = document.getElementById("q4").value;
-        // var q5 = document.getElementById("q5").value;
-        // var q6 = document.getElementById("q6").value;
-
+       
         var q1 = document.querySelector('[name=q1]:checked').value;
         var q2 = document.querySelector('[name=q2]:checked').value;
         var q3 = document.querySelector('[name=q3]:checked').value;
@@ -54,8 +34,7 @@ $(document).ready(function() {
         userAnswerArray.push(q5);
         userAnswerArray.push(q6);
 
-        // console.log(q1, q2, q3, q4, q5, q6);
-        console.log(q1, q2, q3, q4, q5, q6);
+       
 
 
         for (var i = 0; i < answers.length; i++) {
@@ -68,50 +47,12 @@ $(document).ready(function() {
         }
 
 
-        // if (q1 === q1Answer) {
-        //     correct++;
-        // }
-        // else {
-        //     incorrect++;
-        // }
-
-        // if (q2 === q2Answer) {
-        //     correct++;
-        // }
-        // else {
-        //     incorrect++;
-        // }
-
-        // if (q3 === q3Answer) {
-        //     correct++;
-        // }
-        // else {
-        //     incorrect++;
-        // }
-
-        // if (q4 === q4Answer) {
-        //     correct++;
-        // }
-        // else {
-        //     incorrect++;
-        // }
-
-        // if (q5 === q5Answer) {
-        //     correct++;
-        // }
-        // else {
-        //     incorrect++;
-        // }
-
-        // if (q6 === q6Answer) {
-        //     correct++;
-        // }
-        // else {
-        //     incorrect++;
-        // }
 
         console.log("You have " + correct + " answers");
         console.log("You have " + incorrect + " incorrect answers");
+
+        $('#correctAnswers').text(correct);
+        $('#incorrectAnswers').text(incorrect);
 
 
 
@@ -122,13 +63,11 @@ $(document).ready(function() {
     }
 
 
-    // $("input[type='button']").on('click', function(){
-    //     var radioValue = $("input[name='q1']:checked").val();
-    //     if(radioValue){
-    //         alert(radioValue);
-    //     }
-    // });
+   
     
+    
+    
+   
 
     
 
